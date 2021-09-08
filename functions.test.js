fca94dfd170cc89f2dc5df8a02ef9bc261e1f561
ChrisVaz1995 <christopher.vazquez2015@gmail.com>
2021-09-08 14:22:01 -0500

    updated files

---
+++ functions.test.js
@@ -0,0 +1,29 @@
+const {returnTwo, greeting, add, multiply, subtract, divide} = require("./functions");
+
+test("returnTwo should return 2", () => {
+    expect(returnTwo(2)).toEqual(2);
+});
+
+test("return the greeting", () => {
+    expect(greeting('Chris')).toEqual(`Hello, Chris`);
+});
+
+test("return sum", () => {
+    expect(add(1,2)).toEqual(3);
+    expect(add(5,9)).toEqual(14);
+});
+
+test("return sum of multiplication", () => {
+    expect(multiply(5,2)).toEqual(10);
+    expect(multiply(6,2)).toEqual(12);
+});
+
+test("return sum of division", () => {
+    expect(divide(6,2)).toEqual(3);
+    expect(divide(10,5)).toEqual(2);
+});
+
+test("return sum of subtraction", () => {
+    expect(subtract(10,5)).toEqual(5);
+    expect(subtract(12,6)).toEqual(6);
+});
